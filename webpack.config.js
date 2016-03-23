4dcfed45806449fb45ef241197dc3612feb8ce38
Adrian Tello <adriantl7@gmail.com>
2016-03-23 21:14:06 +0100

    Add support for es6

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,18 @@ module.exports = {
     entry: {
         map: "./src/map.js"
     },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
     output: {
         filename: '[name].js',
         path: __dirname + '/web/build'
