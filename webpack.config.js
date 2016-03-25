68454c429e8bb9bf5c545f40773c91bfe6e9290f
Adrian Tello <adriantl7@gmail.com>
2016-03-25 18:25:05 +0100

    Webpack: Enable support for jsx

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
                 exclude: /(node_modules)/,
                 loader: 'babel',
                 query: {
-                    presets: ['es2015']
+                    cacheDirectory: true,
+                    presets: ['react', 'es2015']
                 }
             },
             {
