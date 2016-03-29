1b30b2061919096d39b2ebab3cdd760f7eeba4e1
Adrian Tello <adriantl7@gmail.com>
2016-03-29 20:10:55 +0200

    Externalize GridBg default vertex shader

--- src/map/objects/GridBg.js
+++ src/map/objects/GridBg.js
@@ -2,13 +2,15 @@
  * Created by tello on 25/03/2016.
  */
 import gridBgFs from '../shaders/gridBg.fs';
+import gridBgVs from '../shaders/gridBg.vs';
 import InstancedHexagonBufferGeometry from '../geometries/InstancedHexagonBufferGeometry';
 import THREE from 'three';
 
 const GridBg = function (numX, numY){
     const geometry = new InstancedHexagonBufferGeometry(numX, numY);
     const material = new THREE.ShaderMaterial({
-        fragmentShader: gridBgFs
+        fragmentShader: gridBgFs,
+        vertexShader: gridBgVs
     });
 
     THREE.Mesh.call(this, geometry, material);
