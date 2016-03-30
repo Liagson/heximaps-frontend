b4432d4a4ecf3545e1937385920dad3a9c9322d6
Adrian Tello <adriantl7@gmail.com>
2016-03-30 23:29:38 +0200

    Map the state to the Root container

--- src/map/containers/Root.dev.js
+++ src/map/containers/Root.dev.js
@@ -1,18 +1,28 @@
 /**
  * Created by tello on 30/03/2016.
  */
+import { connect } from 'react-redux'
 import DevTools from '../containers/DevTools';
 import Map from '../components/Map';
 import { Provider } from 'react-redux'
 import React, { Component } from 'react';
 
-export default class Root extends Component {
+class Root extends Component {
     render(){
+        const mapSize = this.props.map.get('size');
+        const mapTiles = this.props.map.get('tiles');
+
         return <div className="root-container">
             <Provider store={this.props.store}>
-                <Map/>
+                <Map size={mapSize} tiles={mapTiles}/>
             </Provider>
             <DevTools store={this.props.store}/>
         </div>;
     }
 }
+
+export default connect(function(state){
+    return {
+        map: state.get('map')
+    };
+})(Root);
--- src/map/containers/Root.prod.js
+++ src/map/containers/Root.prod.js
@@ -5,12 +5,21 @@ import Map from '../components/Map';
 import { Provider } from 'react-redux'
 import React, { Component } from 'react';
 
-export default class Root extends Component {
+class Root extends Component {
     render(){
+        const mapSize = this.props.map.get('size');
+        const mapTiles = this.props.map.get('tiles');
+
         return <div className="root-container">
                 <Provider store={this.props.store}>
-                    <Map/>
+                    <Map size={mapSize} tiles={mapTiles}/>
                 </Provider>
             </div>;
     }
 }
+
+export default connect(function(state){
+    return {
+        map: state.get('map')
+    };
+})(Root);
