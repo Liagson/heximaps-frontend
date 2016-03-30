3cd357baa76eda58b1748705b20019e97fca478a
Adrian Tello <adriantl7@gmail.com>
2016-03-30 23:03:38 +0200

    Initialize model

--- src/map/store/reducers/map.js
+++ src/map/store/reducers/map.js
@@ -1,8 +1,25 @@
 /**
  * Created by tello on 29/03/2016.
  */
+import Immutable from 'immutable';
+
 const map = function(state = [], action){
     switch (action.type) {
+        case "@@INIT":
+            return Immutable.fromJS({
+                size: {
+                    x: 5,
+                    y: 5
+                },
+                tiles: [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0]
+                ]
+            });
+            break;
         default:
             return state;
     }
