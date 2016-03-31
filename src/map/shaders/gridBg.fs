89e8650c0db4b56c15f08f9f8af8b840f746999f
Adrian Tello <adriantl7@gmail.com>
2016-03-31 19:47:03 +0200

    Set the red color based on the tile type

--- src/map/shaders/gridBg.fs
+++ src/map/shaders/gridBg.fs
@@ -1,5 +1,5 @@
-uniform vec3 color;
+varying float fTileType;
 
 void main() {
-    gl_FragColor = vec4(color, 1.0);
+    gl_FragColor = vec4(fTileType / 5.0, 0.0, 0.0, 1.0); //At the moment 5 types
 }
