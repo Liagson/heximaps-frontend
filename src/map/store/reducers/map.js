628d391ae415a9f2f5f87de4025f6f876c16d660
Adrian Tello <adriantl7@gmail.com>
2016-04-16 19:57:05 +0200

    Generate random initial maps

--- src/map/store/reducers/map.js
+++ src/map/store/reducers/map.js
@@ -7,22 +7,34 @@ import Immutable from 'immutable';
 const map = function(state = [], action){
     switch (action.type) {
         case "@@INIT":
+            const sizeX = 40;
+            const sizeY = 30;
+
+            const tiles = [];
+            //Populate map with random tiles
+            for(let y = 0; y < sizeY; y++){
+                const rowTiles = [];
+
+                for(let x = 0; x < sizeX; x++){
+                    //Get random tile type
+                    const randomTileType = Math.floor((Math.random() * 5));
+
+                    rowTiles.push(randomTileType);
+                }
+
+                tiles.push(rowTiles);
+            }
+
             return Immutable.fromJS({
                 cursor: {
                     x: 0,
                     y: 0
                 },
                 size: {
-                    x: 5,
-                    y: 5
+                    x: sizeX,
+                    y: sizeY
                 },
-                tiles: [
-                    [0, 1, 2, 3, 4],
-                    [2, 4, 1, 0, 3],
-                    [3, 1, 2, 4, 4],
-                    [0, 4, 3, 0, 1],
-                    [1, 3, 0, 2, 0]
-                ]
+                tiles: tiles
             });
             break;
         case CURSOR_CHANGE:
